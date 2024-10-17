af4804c2cd80db30954807c00a80e26848cb229c
johndonor3 <j.donor@tcu.edu>
2024-10-17 15:46:06 +0000

    dummy orig exp done

--- python/roboscheduler/fields.py
+++ python/roboscheduler/fields.py
@@ -344,6 +344,7 @@ class Fields(object, metaclass=FieldsSingleton):
                         ('flag', np.int32),
                         ('base_priority', np.int32),
                         ('slots_exposures', np.int32, (24, 2)),
+                        ('original_exposures_done', np.int32, (1)),
                         ('cadence', np.dtype('a25'))]
 
         versionDB = targetdb.Version()
